c99d8ea77c784cbc3cbace853b0322e795e8353b
Josue Gabuardi <josuegabuardi@gmail.com>
2019-05-03 22:46:45 -0600

    separete logic in classes

---
+++ bo3_parser.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+
+class Bo3Parser:
+
+    df = None
+    matches = []
+    teams = {
+        'DFM': 'DetonatioN FocusMe',
+        'INT': 'INTZ e-Sports Club',
+        'VEG': 'Vega Squadron',
+        'MEG': 'MEGA Esports',
+    }
+
+    def __init__(self, file_name, matches):
+        self.df = pd.read_excel('excel_files/' + file_name + '.xlsx', sheet_name='Respuestas de formulario 1')
+        self.matches = matches
+
+    def parse(self):
+        return {'user_predicts': self.get_user_predicts()}
+
+    def get_user_predicts(self):
+        user_predicts = []
+        for row in range(self.df.iloc[:, 0].count() - 1):
+
+            teams_predictions = []
+            for column in range(2, len(self.matches) + 1):
+                if self.df.iloc[row, column] == self.teams[self.matches[column - 2]['team1']]:
+                    teams_predictions.append(1)
+                else:
+                    teams_predictions.append(2)
+
+            user_predicts.append({
+                'user_name': self.df.iloc[row, 1],
+                'pts': 0,
+                'states': [],
+                'team_predictions': teams_predictions
+            })
+        return user_predicts
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import pandas as pd
+from bo3_parser import Bo3Parser
 
-df = pd.read_excel('excel_files/MSI 2019  - Play-In Día 3 (respuestas).xlsx', sheet_name='Respuestas de formulario 1')
+
+# BO3 PARSER ------------------------------------------------------------------
 
 matches = [
     {'team1': 'DFM', 'team2': 'INT'},
@@ -13,29 +14,6 @@ matches = [
     {'team1': 'MEG', 'team2': 'VEG'},
 ]
 
-teams = {
-    'DFM': 'DetonatioN FocusMe',
-    'INT': 'INTZ e-Sports Club',
-    'VEG': 'Vega Squadron',
-    'MEG': 'MEGA Esports',
-}
+predicts = Bo3Parser('MSI 2019  - Play-In Día 3 (respuestas)', matches)
 
-user_predicts = []
-
-for row in range(df.iloc[:, 0].count() - 1):
-
-    teams_predictions = []
-    for column in range(2, len(matches) + 1):
-        if df.iloc[row, column] == teams[matches[column - 2]['team1']]:
-            teams_predictions.append(1)
-        else:
-            teams_predictions.append(2)
-
-    user_predicts.append({
-        'user_name': df.iloc[row, 1],
-        'pts': 0,
-        'states': [],
-        'team_predictions': teams_predictions
-    })
-
-print(user_predicts)
+print(predicts.parse())
